77e30d612423970e005443f12951529cd55dcebf
chanupa <chanupadeshan2002@gmail.com>
2026-02-09 09:06:52 +0530

    core_api

---
+++ core_api/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Truck Container System API is running"})
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('admin/', admin.site.urls),
+    path('api/', include('damage_detection.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
